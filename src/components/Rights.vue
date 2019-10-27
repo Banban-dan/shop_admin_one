c0d818057d27a9a146a244999e325cc64e850ce7
Banban-dan <1583974549@qq.com>
2019-10-27 19:45:25 +0800

    权限列表完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="rights">
     <el-breadcrumb separator-class="el-icon-arrow-right">
-    <el-table-column
-      type="index"
-      width="50">
       <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
       <el-breadcrumb-item>权限列表</el-breadcrumb-item>
     </el-breadcrumb>
     <el-table :data="rightList">
+    <el-table-column
+      type="index"
+      width="50">
+    </el-table-column>
       <el-table-column label="权限名称" prop="authName"></el-table-column>
       <el-table-column label="路径" prop="path"></el-table-column>
       <el-table-column label="层级" prop="level">
