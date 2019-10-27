f17f9229c285a683cb2f9edaf8a44174553f4635
Banban-dan <1583974549@qq.com>
2019-10-27 19:38:45 +0800

    权限列表完成

--- src/components/Rights.vue
+++ src/components/Rights.vue
@@ -1,15 +1,53 @@
 <template>
   <div class="rights">
-    这是rights组件
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+    <el-table-column
+      type="index"
+      width="50">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-table :data="rightList">
+      <el-table-column label="权限名称" prop="authName"></el-table-column>
+      <el-table-column label="路径" prop="path"></el-table-column>
+      <el-table-column label="层级" prop="level">
+        <template v-slot:default= "{ row }">
+          <p v-if="row.level === '0'">一级</p>
+          <p v-if="row.level === '1'">二级</p>
+          <p v-if="row.level === '2'">三级</p>
+        </template>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      rightList: []
+    }
+  },
+  async created () {
+    const { data, meta } = await this.$axios.get('rights/list')
+    if (meta.status === 200) {
+      this.rightList = data
+      console.log(this.rightList)
+    } else {
+      this.$message.error(meta.$message)
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  .rights {
+    .el-breadcrumb {
+      height: 40px;
+      line-height: 40px;
+      margin-bottom: 10px;
+      border-bottom: 1px solid #ccc;
+    }
+  }
 </style>
